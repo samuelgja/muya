729e16fb92f2dc057db3eb037395a4a2519b4937
samuel.gjabel <samuel.gjabel@gmail.com>
2024-11-17 17:21:50 +0700

    chore: remove jest.config - use bun instead

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  transform: {
-    '^.+\\.jsx?$': 'babel-jest', // Adding this line solved the issue
-    '^.+\\.tsx?$': 'ts-jest',
-  },
-}
